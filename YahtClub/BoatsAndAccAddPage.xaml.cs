44bbf2867236cfaa63bc19f76dbb15b003ba6ed4
alex16541 <aleks00.7@bk.ru>
2021-04-22 10:57:35 +0300

    Доделал Fit

--- YahtClub/BoatsAndAccAddPage.xaml.cs
+++ YahtClub/BoatsAndAccAddPage.xaml.cs
@@ -18,11 +18,70 @@ namespace YahtClub
     /// <summary>
     /// Логика взаимодействия для BoatsAndAccAddPage.xaml
     /// </summary>
-    public partial class BoatsAndAccAddPage : Page
+    public partial class BoatsAndAccAddPage : Window
     {
-        public BoatsAndAccAddPage()
+        public Entities db { get; set; }
+        Fit fit = new Fit();
+        public int fitId = "";
+        public bool isEditable = false;
+        public BoatsAndAccAddPage(Entities db)
         {
             InitializeComponent();
+            this.db = db;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Top = (1080 - this.Height) / 2;
+            this.Left = (1920 - this.Width) / 2;
+            cbBoat.ItemsSource = db.Boats.ToList();
+            cbBoat.SelectedValuePath = "boat_ID";
+            cbBoat.DisplayMemberPath = "Model";
+            cbAccessory.ItemsSource = db.Accessory.ToList();
+            cbAccessory.SelectedValuePath = "Accessory_ID";
+            cbAccessory.DisplayMemberPath = "AccName";
+
+            if (isEditable)
+            {
+                fit = db.Fit.Where(f => f.Fit_ID == fitId).FirstOrDefault();
+                cbBoat.SelectedValue = fit.Boat_ID;
+                cbAccessory.SelectedValue = fit.Accessory_ID;
+                btnAdd.Content = "Edit";
+                Title = "Edit boat";
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbBoat.Text != "" && cbAccessory.Text != "")
+            {
+                try
+                {
+                    fit.Boat_ID = int.Parse(cbBoat.SelectedValue.ToString());
+                    fit.Accessory_ID = int.Parse(cbAccessory.SelectedValue.ToString());
+                }
+                catch (Exception exep)
+                {
+                    MessageBox.Show(exep.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+
+                if (db.Fit.Where(f => f.Boat_ID == int.Parse(cbBoat.SelectedValue.ToString()) && f.Accessory_ID == int.Parse(cbAccessory.SelectedValue.ToString())).FirstOrDefault() == null)
+                {
+                    db.Fit.Add(fit);
+                }
+                else MessageBox.Show("Такая запсь уже существует.", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+                db.SaveChanges();
+                DialogResult = true;
+                this.Close();
+            }
+            else MessageBox.Show("Все поля должны быть заполнены.", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- YahtClub/BoatsAndAccPage.xaml.cs
+++ YahtClub/BoatsAndAccPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.Entity;
 
 namespace YahtClub
 {
@@ -20,9 +21,64 @@ namespace YahtClub
     /// </summary>
     public partial class BoatsAndAccPage : Page
     {
+        Entities db = new Entities();
         public BoatsAndAccPage()
         {
             InitializeComponent();
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            db.Boats.Load();
+            db.Fit.Load();
+            db.Accessory.Load();
+            dgFit.ItemsSource = db.Fit.ToList();
+            dgFit.SelectedValuePath = "Fit_ID";
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            if (new BoatsAndAccAddPage(db) { isEditable = true, fitId = dgFit.SelectedValue.ToString() }.ShowDialog() == true)
+            {
+                dgRefresh();
+            }
+        }
+
+        private void btnDelate_Click(object sender, RoutedEventArgs e)
+        {
+            var fit = db.Fit.Where(f => f.Fit_ID == int.Parse(dgFit.SelectedValue.ToString())).FirstOrDefault();
+            if (MessageBox.Show(
+                "Вы точно хотите удалить эту запись?",
+                "Внимание!",
+                MessageBoxButton.YesNoCancel,
+                MessageBoxImage.Question
+                ) == MessageBoxResult.Yes)
+            {
+                db.Fit.Remove(fit);
+                db.SaveChanges();
+                dgRefresh();
+            }
+        }
+
+        private void btnAddNewFit_Click(object sender, RoutedEventArgs e)
+        {
+            if (new BoatAddWindow(db).ShowDialog() == true)
+            {
+                dgRefresh();
+            }
+        }
+
+        private void dgRefresh()
+        {
+
+            dgFit.ItemsSource = db.Fit.ToList();
+            dgFit.Items.Refresh();
         }
     }
 }
